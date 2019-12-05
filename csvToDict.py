77ae2d205676526d503022aaff241eed4b3551c9
ackSec <jonmedina@gmail.com>
2019-12-05 10:24:59 -0800

    update

--- csvToDict.py
+++ csvToDict.py
@@ -3,5 +3,5 @@ import csv
 with open(r"SampleData.csv", 'r+') as f:
     reader = csv.DictReader(f, delimiter=',')
     for row in reader:
-        print(row)
-    print(f'Header: {reader.fieldnames}')
+        print(row, file=open("output.txt", "a"))
+    print(f'Header: {reader.fieldnames}', file=open("output.txt", "a"))
